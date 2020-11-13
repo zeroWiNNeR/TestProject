0b3f66ae52ba2c8b08b965628fcb211a920a0db2
zeroWiNNeR <vekovshinin.ayu@gmail.com>
2020-11-13 13:10:19 +0500

    Add initializing test_db at application start.

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -2,11 +2,31 @@ package com.example.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.ResourceLoader;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 @SpringBootApplication
 public class TestApplication {
 
     public static void main(String[] args) {
+
+        String sql = "CREATE DATABASE test_db";
+        try (Connection db = DriverManager.getConnection("jdbc:postgresql://localhost:5432/", "postgres", "postgres")) {
+            Statement stmt = db.createStatement();
+            stmt.execute(sql);
+        } catch (Exception e) {
+            e.getLocalizedMessage();
+            System.out.println(e.toString());
+        }
+
         SpringApplication.run(TestApplication.class, args);
     }
 
